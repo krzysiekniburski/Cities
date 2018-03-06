19f00119356a37dd77697348cc40b77e4fe6ff58
Student <Student@B3-Lab>
2018-03-06 12:47:03 +0100

    Struktura projektu

---
+++ Shop2018/src/shop2018/Shop2018.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shop2018;
+
+/**
+ *
+ * @author Student
+ */
+public class Shop2018 {
+
+    /**
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
