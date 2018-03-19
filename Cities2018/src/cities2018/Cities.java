273fbf9df034c2aa89efcbb052da16be8ed291f6
krzysiekniburski <36968920+krzysiekniburski@users.noreply.github.com>
2018-03-20 00:06:55 +0100

    Update Cities.java
    Dodane metody cities, provinceWithOneCity, ktoreMiasto.

--- Cities2018/src/cities2018/Cities.java
+++ Cities2018/src/cities2018/Cities.java
@@ -27,13 +27,32 @@ public Collection<String> cities(String province)
 {
     return provinces.get(provinces);
 }
+public String ktoreMiasto(String province,int n)
+{
+    List<String> pom = new ArrayList();
+    pom.addAll(provinces.get(province));
+    return pom.get(n);
+}
 public Map<String,String> provinceWithOneCity()
 {
-    return null;
+    Map<String,String> one = new HashMap();
+    for(String key : provinces.keySet())
+    {
+        if(provinces.get(key).size()==1)
+        {
+            one.put(key,ktoreMiasto(key,0));
+        }
+    }
+    return one;
 }
 public Collection<String> cities()
 {
-    return null;
+    List<String> m = new ArrayList();
+    for(String key : provinces.keySet())
+    {
+       m.addAll(provinces.get(key));
+    }
+    return m;
 }
 public static void main(String[] args)
 {
